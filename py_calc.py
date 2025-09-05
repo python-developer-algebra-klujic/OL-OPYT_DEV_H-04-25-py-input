3dfc0e529a4b0e84f4f94ed4f2d4c9cd3d23b56b
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-05 18:19:01 +0200

    Create py_calc.py

---
+++ py_calc.py
@@ -0,0 +1,16 @@
+'''
+Zatražite od korisnika unos dva broja. 
+Nakon unosa brojeva, ispišite:
+    zbroj, razliku, umnožak, količnik (rezultat djeljnja), 
+    potenciranje te modulo unesenih brojeva
+
+Svaka operacija treba biti ispisana u novom redu, a ispis treba imati uključene brojeve, 
+znak računske operacije te rezultat.
+PRIMJER ISPISA: 
+5 + 8 = 13
+5 - 8 = -3
+
+NAPOMENA Za sada kod unosa neka kod prvog unosa drugi broj NE bude 0 (nula), 
+jer nije dopušteno dijeliti s nulom. To svakako pokušajte napraviti, ali NE u prvom pokušaju.
+'''
+
