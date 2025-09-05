10cb8f9913bcede0288f2e0b12eaf725a6b0cd72
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-05 19:25:42 +0200

    Update py_calc.py

--- py_calc.py
+++ py_calc.py
@@ -14,16 +14,32 @@ NAPOMENA Za sada kod unosa neka kod prvog unosa drugi broj NE bude 0 (nula),
 jer nije dopušteno dijeliti s nulom. To svakako pokušajte napraviti, ali NE u prvom pokušaju.
 '''
 
+# 1. korak - pohranjujemo vrijednosti od korisnika
 a = input('Upisite prvi broj: ')
 b = input('Upisite drugi broj: ')
+
+# 2. korak - Unesene brojke trebamo pretvoriti u brojeve kako bismo mogli racunati s njima
+a = int(a)
+b = int(b)
 
 
 # print('Vrijednost a:', a)
 # print('Vrijednost b:', b)
 
+# 3. korak - ispisujemo vrijednosti
 print('Zbroj brojeva a', a, 'i b', b, 'je', a + b)
 print('Razlika brojeva a', a, 'i b', b, 'je', a - b)
 print('Umnozak brojeva a', a, 'i b', b, 'je', a * b)
 print('Kolicnik brojeva a', a, 'i b', b, 'je', a / b)
 print('Potencija brojeva a', a, 'i b', b, 'je', a ** b)
-print('Modula brojeva a', a, 'i b', b, 'je', a % b)
+print('Modulo brojeva a', a, 'i b', b, 'je', a % b)
+
+'''
+Modulo - simbol %
+
+173 / 5 = 34
+15
+ 23
+ 20
+ (3) -> ovo je ostatak dijeljenja, odnosno modulo
+'''
