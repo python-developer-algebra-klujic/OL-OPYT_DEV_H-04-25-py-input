bce77a6556bb6c7afe86601eb88a7a548e0433ea
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-05 20:15:39 +0200

    Create py_calculator.py

---
+++ py_calculator.py
@@ -0,0 +1,27 @@
+'''
+Zatražite od korisnika unos dva broja. 
+Nakon unosa brojeva, ispišite:
+    zbroj, razliku, umnožak, količnik (rezultat djeljnja), 
+    potenciranje te modulo unesenih brojeva
+
+Svaka operacija treba biti ispisana u novom redu, a ispis treba imati uključene brojeve, 
+znak računske operacije te rezultat.
+PRIMJER ISPISA: 
+5 + 8 = 13
+5 - 8 = -3
+
+NAPOMENA Za sada kod unosa neka kod prvog unosa drugi broj NE bude 0 (nula), 
+jer nije dopušteno dijeliti s nulom. To svakako pokušajte napraviti, ali NE u prvom pokušaju.
+'''
+
+# 1. korak - pohranjujemo vrijednosti od korisnika
+a = float(input('Upisite prvi broj: '))
+b = float(input('Upisite drugi broj: '))
+
+# 2. korak - ispisujemo vrijednosti
+# print('Zbroj brojeva a', a, 'i b', b, 'je', a + b)
+# print('Razlika brojeva a', a, 'i b', b, 'je', a - b)
+# print('Umnozak brojeva a', a, 'i b', b, 'je', a * b)
+# print('Kolicnik brojeva a', a, 'i b', b, 'je', a / b)
+# print('Potencija brojeva a', a, 'i b', b, 'je', a ** b)
+# print('Modulo brojeva a', a, 'i b', b, 'je', a % b)
